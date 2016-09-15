b52312f5161411213e2a1730ed6ed6ecc970d2ed
Jacob van't Hoog <vantjac@gmail.com>
2016-09-15 03:56:59 +0000

    Add functionality to sim

--- threelib/sim/base.py
+++ threelib/sim/base.py
@@ -1,5 +1,8 @@
 __author__ = "jacobvanthoog"
 
+from threelib.vectorMath import Vector
+from threelib.vectorMath import Rotate
+
 class SimObject:
     
     def scan(self, timeElapsed, totalTime):
@@ -59,3 +62,104 @@ class SimObject:
         Should only update state when update() is called.
         """
         return False
+        
+        
+class ActionList:
+    
+    def __init__(self):
+        self.actions = [ ]
+        
+    def addAction(self, action):
+        """
+        action takes a single argument: a List of things to update, which it may
+        add to.
+        """
+        self.actions.append(action)
+        
+    def doActions(self):
+        """
+        Return a List of things to update.
+        """
+        toUpdateList = [ ]
+        for action in self.actions:
+            action(toUpdateList)
+        self.actions = [ ]
+        return toUpdateList
+
+
+class Entity(SimObject):
+    
+    def __init__(self, position=Vector(0, 0, 0), rotation=Rotate(0, 0, 0)):
+        super().__init__()
+        self.actions = ActionList()
+        
+        self.position = position
+        self.rotation = rotation
+        self.parent = None
+        self.children = [ ]
+        
+    def getPosition(self):
+        return self.position
+
+    def getRotation(self):
+        return self.rotation
+        
+    def translate(self, vector, moveChildren=True):
+        # if new children are added after this, they won't be moved
+        if moveChildren:
+            childrenToMove = list(self.children)
+        
+        def do(toUpdateList):
+            self.position += vector
+            if moveChildren:
+                for child in childrenToMove:
+                    child.translate(vector, True)
+                    toUpdateList.append(child)
+        self.actions.addAction(do)
+        
+    def rotate(self, rotate, moveChildren=True):
+        # if new children are added after this, they won't be moved
+        if moveChildren:
+            childrenToMove = list(self.children)
+            selfPosition = self.getPosition()
+        
+        def do(toUpdateList):
+            self.rotation = self.rotation.rotate(rotate)
+            if moveChildren:
+                for child in childrenToMove:
+                    child.rotate(rotate, True)
+                    startPosition = child.getPosition()
+                    endPosition = startPosition.rotateAround(rotate,
+                        selfPosition)
+                    child.translate(endPosition - startPosition)
+                    toUpdateList.append(child)
+        self.actions.addAction(do)
+
+    def getParent(self):
+        return self.parent
+
+    def setParent(self, parent):
+        def do(toUpdateList):
+            self.parent = parent
+        self.actions.addAction(do)
+    
+    def getChildren(self):
+        return self.children
+
+    def addChild(self, child):
+        def do(toUpdateList):
+            self.children.append(child)
+            child.setParent(self)
+            toUpdateList.append(child)
+        self.actions.addAction(do)
+
+    def removeChild(self, child):
+        def do(toUpdateList):
+            self.children.remove(child)
+            child.setParent(None)
+            toUpdateList.append(child)
+        self.actions.addAction(do)
+
+    def update(self, time):
+        return self.actions.doActions()
+        
