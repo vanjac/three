73948c70dd2173a15fff187cb99b48f98605da6e
Jacob van't Hoog <vantjac@gmail.com>
2016-09-18 10:29:32 -0700

    Add methods to EditorObject, document

--- threelib/edit/base.py
+++ threelib/edit/base.py
@@ -6,77 +6,124 @@ from threelib.vectorMath import Rotate
 # for ast.literal_eval, used for parsing properties
 import ast
 
-# abstract class
+
 class EditorObject:
+    """
+    An abstract class for objects that can be manipulated in the editor, and
+    that tie to SimObjects in the World.
+    """
     
     def __init__(self):
         self.name = ""
         self.children = [ ]
         self.parent = None
         self.selected = False
-
-    # return a string
+    
     def getName(self):
+        """
+        Return the name of the EditorObject as a string. Names have no meaning
+        and are for organization only.
+        """
         return self.name
 
     def setName(self, name):
+        """
+        Set the name of the EditorObject.
+        """
         self.name = name
-
-    # string describing the EditorObject
+    
     def getType(self):
-        pass
-
-    # return a Vector
+        """
+        Return a string describing the EditorObject. Override this.
+        """
+        return "EditorObject"
+    
     def getPosition(self):
+        """
+        Return a Vector. Override this.
+        """
         pass
-
-    # return a Rotation
+    
     def getRotation(self):
+        """
+        Return a Rotation. Override this.
+        """
         pass
-
-    # return a tuple of 2 vectors (minCoords, maxCoords). Bounds are relative to
-    # the object position
+    
     def getBounds(self):
+        """
+        Return a tuple of 2 Vectors: (min_coordinates, max_coordinates). Bounds
+        are relative to the object position. Override this.
+        """
         pass
 
     def getCenter(self):
+        """
+        Calculate the center of the bounds of the object.
+        """
         b1, b2 = self.getBounds()
         return (b1 + b2) / 2 + self.getPosition()
 
     def getDimensions(self):
+        """
+        Calculate the dimensions of the object based on the bounds, as a Vector.
+        """
         b1, b2 = self.getBounds()
         return b2 - b1
 
     def setPosition(self, position):
+        """
+        Set the position. Override this.
+        """
         pass
 
     def setRotation(self, rotation):
+        """
+        Set the rotation. Override this.
+        """
         pass
-
-    # apply the current rotation so it becomes 0
+    
     def applyRotation(self):
+        """
+        Apply the current rotation so that it becomes 0. Override this.
+        """
         pass
-
-    # factor is a vector
+    
     def scale(self, factor):
+        """
+        Apply a scale factor to the object. ``factor`` is a Vector. Override
+        this.
+        """
         pass
 
     def getMesh(self):
-        pass
+        """
+        Return the object's mesh if it exists, or None. Override this.
+        """
+        return None
 
-    # Draw the object with the GraphicsTools.
-    # Draw it at the origin with no rotation (but with scaling) -- it is the
-    # editor's responsibility to position and rotate
-    # it.
     def drawObject(self, graphicsTools):
+        """
+        Using the GraphicsTools object, draw this EditorObject at the origin
+        with no rotation (but with scaling) -- it is the editor's responsibility
+        to position and rotate it. Override this.
+        """
         pass
 
-    # color is a tuple of (r, g, b). Draw with OpenGL.
     def drawSelectHull(self, color, graphicsTools):
+        """
+        Similar to ``drawObject()``, draw the select hull of the object in the
+        specified color. This is the area on screen that the user can click to
+        select the object. ``color`` is a tuple of (r, g, b). Override this.
+        """
         pass
 
-    # a dictionary mapping strings to strings
     def getProperties(self):
+        """
+        Get the user-editable properties of this object. Return a dictionary
+        mapping strings to strings. If you override this, join the new
+        properties list with ``super().getProperties()``.
+        """
         properties = { "name": self.getName(),
                        "position": str(self.getPosition()),
                        "rotation": str(self.getRotation()),
@@ -84,6 +131,10 @@ class EditorObject:
         return properties
 
     def setProperties(self, properties):
+        """
+        Set the properties of this object. If you override this, send
+        unrecognized properties to ``super().setProperties()``.
+        """
         for key, value in properties.items():
             if key == "name":
                 self.setName(value)
@@ -92,50 +143,91 @@ class EditorObject:
             if key == "rotation":
                 self.setRotation(Rotate.fromTuple(ast.literal_eval(value)))
 
-    # return another EditorObject
     def getParent(self):
+        """
+        Return the EditorObject that has this object as a child. Set
+        automatically.
+        """
         return self.parent
 
-    # should only be called by other EditorObjects
     def setParent(self, parent):
+        """
+        Internal method, only called by other EditorObjects.
+        """
         self.parent = parent
 
-    # return a list of EditorObjects
     def getChildren(self):
+        """
+        Return a list of other EditorObjects. Children are user-editable.
+        """
         return self.children
 
     def addChild(self, child):
+        """
+        Add a child EditorObject.
+        """
         child.removeFromParent()
         child.setParent(self)
         self.children.append(child)
 
     def removeChild(self, child):
+        """
+        Remove a child EditorObject.
+        """
         child.setParent(None)
         self.children.remove(child)
 
     def removeFromParent(self):
+        """
+        Remove this object as a child from its parent. 
+        """
         if self.parent != None:
             self.parent().removeChild(self)
     
     def isSelected(self):
+        """
+        This ``isSelected`` flag is set automatically.
+        """
         return self.selected
 
     def setSelected(self, selected):
+        """
+        Called by the editor.
+        """
         self.selected = selected
 
-    # clone this object
-    # the new object will NOT have any parent or children.
     def clone(self):
+        """
+        Clone this object. The new object will NOT have a parent or children.
+        Don't override this to add functionality. Instead override
+        ``addToClone``.
+        """
         clone = EditorObject()
         self.addToClone(clone)
         return clone
-        
-    # internal method to add this objects properties to a newly created clone
-    # extending classes should call super().addToClone(clone) first
+    
     def addToClone(self, clone):
+        """
+        Internal method to add this objects properties to a newly created clone.
+        If you override this, call super().addToClone(clone) first, then add
+        your own properties. This implementation sets the name, position, and
+        rotation of the object.
+        """
         clone.setName(self.getName())
         clone.setPosition(self.getPosition())
         clone.setRotation(self.getRotation())
+        
+    def addToWorld(self, world):
+        """
+        Add a representation to the world.
+        """
+        pass
+        
+    def removeFromWorld(self, world):
+        """
+        Remove the representation created by ``addToWorld()``.
+        """
+        pass
 
 
 class WorldObject(EditorObject):
