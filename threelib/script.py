91bb94400ffb06421b9adac5c0722429255085bf
Jacob van't Hoog <vantjac@gmail.com>
2016-09-21 09:47:41 -0700

    Add script module

---
+++ threelib/script.py
@@ -0,0 +1,19 @@
+__author__ = "jacobvanthoog"
+
+localDict = { }
+
+
+def runScript(script):
+    """
+    Run a script. Scripts will share the same local dictionary.
+    """
+    exec(script, globals(), localDict)
+
+def setVariable(script, varName):
+    """
+    Set a variable from the value of the script. Return the value.
+    """
+    value = eval(script, globals(), localDict)
+    localDict[varName] = value
+    return value
+
