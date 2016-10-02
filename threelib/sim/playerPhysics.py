2ea363eeb56c6a5bccee5a64248e41efabb38871
Jacob van't Hoog <vantjac@gmail.com>
2016-10-02 10:54:34 -0700

    Calculate convex hull of CollisionMesh

--- threelib/sim/playerPhysics.py
+++ threelib/sim/playerPhysics.py
@@ -1,6 +1,7 @@
 __author__ = "jacobvanthoog"
 
 from threelib.vectorMath import Vector
+from threelib import vectorMath
 import threelib.sim.base
 
 class CollisionMesh(threelib.sim.base.Entity):
@@ -17,6 +18,72 @@ class CollisionMesh(threelib.sim.base.Entity):
         self.floorStartTouchAction = None
         self.floorEndTouchAction = None
         self.ceilingCollideAction = None
+        
+        # Info about the hull of the object
+        # This information assumes the object is at the origin, with a Z
+        # rotation of 0. However, it DOES factor in the X and Y rotations.
+        # So the object can move and rotate around the Z axis without having to
+        # regenerate the hull, but as soon as it rotates any other way it will
+        # have to recalculate.
+        
+        # a series of 2d vectors in a counterclockwise order that describe the
+        # 2d convex hull of the object on the XY plane
+        self.convexHullPoints = [ ]
+        
+        self._generateHull()
+        
+    def _generateHull(self):
+        # TODO: These algorithms ignore the X and Y rotation of the object!
+    
+        # find the 2d convex hull with Jarvis's algorithm
+        # geeksforgeeks.org/convex-hull-set-1-jarviss-algorithm-or-wrapping
+        
+        self.convexHullPoints = [ ]
+        if len(self.mesh.getVertices()) > 0:
+            vertices = list(self.mesh.getVertices())
+            nextVertex = vertices[0]
+            
+            while len(vertices) != 0:
+                vertices.remove(nextVertex)
+                self.convexHullPoints.append(nextVertex.getPosition().setZ(0))
+                
+                if len(vertices) == 0:
+                    break
+                
+                verticesToRemove = [ ]
+                nextVertexCandidate = None
+                for vertex in vertices:
+                    pos = vertex.getPosition().setZ(0)
+                    
+                    if vertex.getPosition().setZ(0) \
+                            .isClose(nextVertex.getPosition().setZ(0)):
+                        verticesToRemove.append(vertex)
+                    elif nextVertexCandidate == None:
+                        nextVertexCandidate = vertex
+                    else:
+                        # find the 2d orientation of the vectors
+                        orientation = self._orientation(
+                            nextVertex.getPosition(), vertex.getPosition(),
+                            nextVertexCandidate.getPosition())
+                        if orientation == 2:
+                            nextVertexCandidate = vertex
+                for vertex in verticesToRemove:
+                    vertices.remove(vertex)
+                nextVertex = nextVertexCandidate
+        
+        print("Convex hull points:", self.convexHullPoints)
+                        
+    def _orientation(self, p, q, r):
+        # for 2d vectors (Z coordinate is ignored)
+        # return 0 for colinear, 1 for clockwise, 2 for counterclockwise
+        # geeksforgeeks.org/convex-hull-set-1-jarviss-algorithm-or-wrapping
+        val = (q.y - p.y) * (r.x - q.x) - \
+              (q.x - p.x) * (r.y - q.y)
+ 
+        if vectorMath.isclose(val, 0):
+            return 0
+        return 1 if val > 0 else 2
+        
         
     def getMesh(self):
         """
