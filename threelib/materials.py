48c5b743b6147a65f3f5c824046016afe4cbf316
Jacob van't Hoog <vantjac@gmail.com>
2016-10-30 21:18:44 -0700

    Document and clean up materials.py

--- threelib/materials.py
+++ threelib/materials.py
@@ -15,31 +15,51 @@ def isPowerOf2(num):
     return num != 0 and ((num & (num - 1)) == 0)
 
 class Texture:
-    
+    """
+    A texture image
+    """
     def __init__(self, data, dataType, xLen, yLen):
         self.data = data
         self.dataType = dataType
         self.xLen = xLen
         self.yLen = yLen
     
-    # for RGBA mode:
-    # an array of chars, of size xLen * yLen * 4, ordered RGBA
     def getData(self):
+        """
+        An array of chars, of size xLen * yLen * numChannels.
+        """
         return self.data
 
     def getXLen(self):
+        """
+        Return the width of the image in pixels.
+        """
         return self.xLen
 
     def getYLen(self):
+        """
+        Return the height of the image in pixels.
+        """
         return self.yLen
 
     def getDataType(self):
+        """
+        The data type of the image, as a string. Values correspond with PIL
+        image modes (like "RGB" or "RGBA").
+        """
         return self.dataType
 
 
 class MaterialReference(Resource):
+    """
+    A material that can be painted onto a mesh face.
+    """
     
-    def __init__(self, name, load=False):
+    def __init__(self, name):
+        """
+        Create a material with the specified name. The name is a path relative
+        to the materials directory, without a file extension.
+        """
         super().__init__()
         self.name = name
         self.number = 0
@@ -47,32 +67,54 @@ class MaterialReference(Resource):
         self.aspectRatio = 1.0
 
     def getName(self):
+        """
+        Get the name of the material. The name is a path relative to the
+        materials directory, without a file extension.
+        """
         return self.name
-
-    # used by 3d rendering
-    # for example, for OpenGL this is the texture object's "name"
     
     def getNumber(self):
+        """
+        Return the number stored with ``setNumber``.
+        """
         return self.number
 
     def setNumber(self, number):
+        """
+        Set an ID number to be associated with this material. This is intended
+        to be used by 3d rendering; for example, for OpenGL this could store the
+        texture object's "name."
+        """
         self.number = number
-
-    # used by 3d rendering
-    # flag to determine if texture has been loaded
+    
     def isLoaded(self):
+        """
+        Check if the material loaded flag was set by ``setLoaded``.
+        """
         return self.loaded
 
     def setLoaded(self, loaded=True):
+        """
+        Set a flag for whether the texture has been loaded. For 3d rendering.
+        """
         self.loaded = loaded
 
     def getAspectRatio(self):
+        """
+        Get the aspect ratio of the texture, as a ratio of the width to the
+        height.
+        """
         return self.aspectRatio
 
     def hasAlbedoTexture(self):
+        # TODO: what is this used for?
         return self.hasTexture
 
     def loadAlbedoTexture(self):
+        """
+        Load the albedo texture for this material, and return a Texture object.
+        The albedo is the base color of the material.
+        """
         materialPath = files.getMaterial(self.name)
         if materialPath == None:
             print("Material not found:", self.name)
@@ -102,3 +144,4 @@ class MaterialReference(Resource):
         self.hasTexture = True
         self.aspectRatio = float(xLen) / float(yLen)
         return Texture(texture, image.mode, xLen, yLen)
+
