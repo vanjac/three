d5fa6689383f1dfd64e0ba49258017c73f89e5c3
Jacob van't Hoog <vantjac@gmail.com>
2016-11-24 11:57:29 -0800

    Change to editor state version 1.1, with converter

--- threelib/edit/fileVersions/converters.py
+++ threelib/edit/fileVersions/converters.py
@@ -16,3 +16,17 @@ def convert_0_0_to_1_0(state):
     state.MAJOR_VERSION = 1
     state.MINOR_VERSION = 0
     return state
+
+@ editorStateConverter(1, 0)
+def convert_1_0_to_1_1(state):
+    for o in state.objects:
+        if o.getMesh() != None:
+            mesh = o.getMesh()
+            for face in mesh.getFaces():
+                face.normal = None
+                face.normalUpdated = False
+
+    state.MAJOR_VERSION = 1
+    state.MINOR_VERSION = 1
+    return state
+
