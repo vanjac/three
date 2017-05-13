1c6f3b5d2e40915026ac0795d4b6b515dc9e2661
Jacob van't Hoog <vantjac@gmail.com>
2017-05-12 21:24:52 -0700

    New FirstPersonPlayer design for more realistic movement; mostly fixes #107

--- threelib/sim/tools/firstPerson.py
+++ threelib/sim/tools/firstPerson.py
@@ -8,7 +8,6 @@ from threelib import vectorMath
 from threelib.sim.input import ButtonInput
 
 class FirstPersonPlayer(PhysicsObject):
-
     def __init__(self, world, xLookAxis, yLookAxis, xWalkAxis, yWalkAxis,
                  jumpButton,
                  cameraHeight=17.0, playerHeight=18.0, playerWidth=8.0,
@@ -24,99 +23,128 @@ class FirstPersonPlayer(PhysicsObject):
         self.yWalkAxis = yWalkAxis
         self.jumpButton = jumpButton
 
-        self.sliding = False
-
         self.walkSpeed = walkSpeed
         self.fallMoveSpeed = fallMoveSpeed
         self.walkDeceleration = walkDeceleration
         self.fallDeceleration = fallDeceleration
         self.maxWalkAngle = maxWalkAngle # in degrees
-        self.minWalkNormalZ = Vector(1.0, 0.0)\
-            .rotate2(math.radians(self.maxWalkAngle)).x
+        if maxWalkAngle == None:
+            self.minWalkNormalZ = None
+        else:
+            self.minWalkNormalZ = Vector(1.0, 0.0)\
+                .rotate2(math.radians(self.maxWalkAngle)).x
         self.jumpVelocity = jumpVelocity
+
+        self.floorXYVelocity = Vector(0, 0, 0)
 
     def scan(self, timeElapsed, totalTime):
         self.scanStart()
+        self.newFloorXYVelocity = self.floorXYVelocity
 
         # look
         self.rotationChange = Rotate(0, -float(self.yLookAxis.getChange()),
                                    -float(self.xLookAxis.getChange()))
-
-        inputTranslation = Vector( self.yWalkAxis.getValue(),
-            -self.xWalkAxis.getValue()).limitMagnitude(1.0) * self.walkSpeed
-
-        # orient walking direction to look direction
-        inputTranslation = inputTranslation.rotate2(self.rotation.z)
-        if self.newCurrentFloor is None:
-            # slower movement while in the air
-            inputTranslation *= float(self.fallMoveSpeed) / self.walkSpeed
-        if self.newXYVelocity.magnitude() < inputTranslation.magnitude() \
-                and not self.sliding:
-            walking = True
-            self.newXYVelocity = inputTranslation
-        else:
-            walking = False
-        if ((not walking) and (not self.sliding)):
-            # when not moving, gradually slow down x-y velocity
-            if self.newCurrentFloor is None:
-                self.newXYVelocity *= (self.fallDeceleration ** timeElapsed)
-            else:
-                self.newXYVelocity *= (self.walkDeceleration ** timeElapsed)
-
-        movement = self.newXYVelocity * timeElapsed
-        self.newSliding = False
-
-        # uphill slopes should slow down movement
-        # downhill slopes should speed up movement
-        slopeFactor = 1.0
-        if self.newCurrentFloor is not None:
-            point = self._topPoint(self.newCurrentFloor,
-                                   self.position + self.positionChange)
-            if point is not None:
-                # if slope is too steep, slide down it
-                if point.normal.z < self.minWalkNormalZ:
-                    self.newSliding = True
-
-                    movementDirection = point.normal.setZ(0).setMagnitude(1.0)
-                    if walking or not self.sliding:
-                        velocity = self.newXYVelocity.setZ(self.newZVelocity)
-                        velocity = velocity.projectOnPlane(point.normal)
-
-                        self.newXYVelocity = velocity.setZ(0)
-                    self.newXYVelocity += movementDirection \
-                        * -PhysicsObject.GRAVITY \
-                        * point.normal.project(Vector(0,0,1)) * timeElapsed \
-                        * (point.normal.setZ(0).magnitude())
-                else:
-                    slopeFactorOffset = \
-                        movement.setMagnitude(1.0).project(point.normal)
-                    if slopeFactorOffset > 0:
-                        slopeFactorOffset = 0
-                    slopeFactor = 1.0 + slopeFactorOffset
 
         jumpEvent = self.jumpButton.getEvent()
         if self.newCurrentFloor is not None \
                 and jumpEvent == ButtonInput.PRESSED_EVENT:
             # jump direction is floor normal
             floorNormal = self._topPoint(self.newCurrentFloor,
-                self.position + self.positionChange).normal
+                                         self.position + self.positionChange).normal
             self.newZVelocity = self.jumpVelocity * floorNormal.z
             self.newXYVelocity += self.jumpVelocity * floorNormal.setZ(0)
             self.newCurrentFloor.doFloorEndTouchAction()
             self.newCurrentFloor = None
 
-        # walk
-        self.positionChange += movement * slopeFactor
+        if self.newCurrentFloor is not None:
+            point = self._topPoint(self.newCurrentFloor,
+                                   self.position + self.positionChange)
+            if self.minWalkNormalZ == None:
+                sliding = True
+            else:
+                sliding = point.normal.z < self.minWalkNormalZ
+        else:
+            point = None
+            sliding = True
+
+        inputTranslation = Vector( self.yWalkAxis.getValue(),
+            -self.xWalkAxis.getValue()).limitMagnitude(1.0) * self.walkSpeed
+        # orient walking direction to look direction
+        inputTranslation = inputTranslation.rotate2(self.rotation.z)
+        if self.newCurrentFloor is None:
+            # slower movement while in the air
+            inputTranslation *= float(self.fallMoveSpeed) / self.walkSpeed
+
+        walking = False
+        if not inputTranslation.isZero():
+            walking = True
+
+            velocity = (inputTranslation * timeElapsed).setZ(0)
+            if self.newCurrentFloor is not None:
+                if not sliding:
+                    # walk; walking up slopes should be slower
+                    project = inputTranslation.setMagnitude(1.0) \
+                        .projectOnPlane(point.normal)
+                    if project.z < 0:
+                        slopeFactor = 1
+                    else:
+                        slopeFactor = project.magnitude()
+                    self.newFloorXYVelocity = inputTranslation * slopeFactor
+                else:
+                    self.newFloorXYVelocity += velocity
+            else:
+                self.newXYVelocity += velocity
+
+        if not walking:
+            # when not moving, gradually slow down x-y velocity
+            if self.newCurrentFloor is None:
+                deceleration = (self.fallDeceleration ** timeElapsed)
+                self.newXYVelocity *= deceleration
+            else:
+                if not sliding:
+                    deceleration = (self.walkDeceleration ** timeElapsed)
+                else:
+                    deceleration = (self.fallDeceleration ** timeElapsed)
+                self.newFloorXYVelocity *= deceleration
+
+        if self.newCurrentFloor is not None:
+            # gravity
+            if sliding or walking:
+                gravityVelocity = Vector(0, 0, PhysicsObject.GRAVITY) \
+                    .projectOnPlane(point.normal)
+                self.newFloorXYVelocity += gravityVelocity.setZ(0) \
+                                               .setMagnitude(
+                    gravityVelocity.magnitude()) * timeElapsed
+
+            velocity = \
+                self.newFloorXYVelocity.setZ(0).projectOnPlane(point.normal)
+            velocity = velocity.setMagnitude(
+                self.newFloorXYVelocity.setZ(0).magnitude())
+            self.newXYVelocity = velocity.setZ(0)
+
+        self.positionChange += self.newXYVelocity * timeElapsed
         if self.newCurrentFloor is None:
             # z velocity
             self.positionChange += Vector(0, 0, self.newZVelocity * timeElapsed)
 
+        prevNewXYVelocity = self.newXYVelocity
         self.scanEnd(timeElapsed, totalTime)
 
+        if (prevNewXYVelocity != self.newXYVelocity
+            or self.currentFloor is None) \
+                and self.newCurrentFloor is not None:
+            point = self._topPoint(self.newCurrentFloor,
+                                   self.position + self.positionChange)
+            velocity = self.newXYVelocity.setZ(self.newZVelocity)
+            velocity = velocity.projectOnPlane(point.normal)
+
+            self.newFloorXYVelocity = velocity.setZ(0)
+
         def do(toUpdateList):
+            self.floorXYVelocity = self.newFloorXYVelocity
+
             prevRotation = self.rotation
             self.rotation += self.rotationChange
-            self.sliding = self.newSliding
 
             # prevent from looking too far up or down
             yRot = self.rotation.y
